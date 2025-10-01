967071c2793f9d41556d328afe677177b7cc3720
smallersoup <837448792@qq.com>
2025-10-01 22:54:59 +0800

    指定读取env

--- test-animation-demo.js
+++ test-animation-demo.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const { toolRegistry } = require('./dist/tools/index.js');
 
 async function testAnimationDemo() {
--- test-animation-export.js
+++ test-animation-export.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const { toolRegistry } = require('./dist/tools/index.js');
 const fs = require('fs');
 const path = require('path');
